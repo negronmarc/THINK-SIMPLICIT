5599c3eb26fd7cd1ae675fa4625985854caada9a
Marc Negron <negronmarc@gmail.com>
2022-06-07 01:45:33 -0400

    added fractional and training to tabs

---
+++ src/components/Container.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import NavTabs from "./NavTabs";
+import About from "./pages/About";
+import Solutions from "./pages/Solutions";
+import Fractional from "./pages/Fractional";
+import Training from "./pages/Training";
+import Resources from "./pages/Resources";
+import Work from "./pages/Work";
+import "./assets/css/container.css";
+
+export default function ThinkContainer() {
+  const [currentPage, setCurrentPage] = useState("Solutions");
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === "Solutions") {
+      return <Solutions />;
+    }
+    if (currentPage === "Fractional") {
+      return <Fractional />;
+    }
+    if (currentPage === "Training") {
+      return <Training />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Resources") {
+      return <Resources />;
+    }
+    return <Work />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <div id="header">
+        <h1 id="website-name">THINK SIMPLICIT</h1>
+        <div id="nav">
+          <NavTabs
+            currentPage={currentPage}
+            handlePageChange={handlePageChange}
+          />
+        </div>
+      </div>
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
---
+++ src/components/NavTabs.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./assets/css/navtabs.css";
+import Dropdown from "react-bootstrap/Dropdown";
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav justify-content-end">
+      <Dropdown>
+        <Dropdown.Toggle id="dropdown-button">SOLUTIONS</Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item
+            href="#Solutions"
+            onClick={() => handlePageChange("Solutions")}
+          >
+            HOME PAGE
+          </Dropdown.Item>
+          <Dropdown.Item
+            href="#Fractional"
+            onClick={() => handlePageChange("Fractional")}
+          >
+            FRACTIONAL SERVICES
+          </Dropdown.Item>
+          <Dropdown.Item
+            href="#Training"
+            onClick={() => handlePageChange("Training")}
+          >
+            TRAINING & DEVELOPMENT
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      {/* <li className="nav-item">
+        <a
+          href="#About"
+          id="navigation"
+          onClick={() => handlePageChange("About")}
+        >
+          About
+        </a>
+      </li> */}
+      {/* <li className="nav-item">
+        <a
+          href="#Resources"
+          id="navigation"
+          onClick={() => handlePageChange("Resources")}
+        >
+          RESOURCES
+        </a>
+      </li> */}
+      <li className="nav-item">
+        <a
+          href="#Workwithus"
+          id="navigation"
+          onClick={() => handlePageChange("Work")}
+        >
+          WORK WITH US
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
